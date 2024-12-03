850aa3e1a2f369d805bc03a0a2a69094a177ba23
dmainagithub <maina2007wambugu@gmail.com>
2024-12-03 12:32:47 +0300

    My Second Change

---
+++ 1_Master_Script_All_Projects.sql
@@ -0,0 +1,32 @@
+--=================================
+-- Credential
+--=================================
+-- Created By:	Daniel Maina Nderitu
+-- Project	:	MADIVA
+-- Year		:	2022 - 2025
+-- Status	:	Data Assessment and Validation - Assessing Linkages Main (Done - 12-5-2023)
+-- Location	:	D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA
+--=================================
+--=================================
+-- Purpose	:	Integrating NCD Datasets: To run this script ensure you have switched on SQlCMD mode (Query > SQLCMD Mode) 
+--              this will enable it to be recognized
+--=================================
+--=================================
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\assess_link_main.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\awigen_i_aphrc.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\diabetics_b.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\diabetics_followup.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\su_clinic_baseline.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\su_clinic_endline.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\SU_First_Clinic_data.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\SU_followup_clinic_data.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\SU_Population_Baseline_data.sql"
+:r "D:\APHRC\GoogleDrive_ii\data_science\madiva\DMAC\databases\tables\MADIVA\MADIVA\SU_Population_Endline_data.sql"
+
+
+
+
+
+
+
+
